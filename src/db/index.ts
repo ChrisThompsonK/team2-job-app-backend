a2911295a2b96604dfd266665b69d718ffe23b19
FilipKainos <filip.szwerluga@kainos.com>
2025-10-07 02:26:00 -0700

    Environment Configuration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,9 @@
+import { config } from "../config/index";
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import * as schema from "./schema";
 
-const sqlite = new Database("./database.sqlite");
+const sqlite = new Database(config.database.url);
 export const db = drizzle(sqlite, { schema });
 
 export type DB = typeof db;
