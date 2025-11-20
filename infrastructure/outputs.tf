9225110ac9477c73e60f1429912bcb0c1c1da7f2
D-sharp-Major <daniel.major@kainos.com>
2025-11-20 13:57:47 +0000

    Bit of role assignment me thinks

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -22,3 +22,21 @@ output "managed_identity_id" {
   value       = azurerm_user_assigned_identity.container_identity.id
   description = "Full resource ID of the managed identity"
 }
+
+output "acr_login_server" {
+  value       = data.azurerm_container_registry.acr.login_server
+  description = "Login server URL for the Azure Container Registry"
+}
+
+output "key_vault_uri" {
+  value       = data.azurerm_key_vault.kv.vault_uri
+  description = "URI of the Key Vault"
+}
+
+output "role_assignments" {
+  value = {
+    acr_pull_id        = azurerm_role_assignment.acr_pull.id
+    kv_secrets_user_id = azurerm_role_assignment.kv_secrets_user.id
+  }
+  description = "IDs of the role assignments created"
+}
