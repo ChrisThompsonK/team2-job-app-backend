80d4a973c99a9d15b63e15fe45c9f5fd3a344e45
D-sharp-Major <daniel.major@kainos.com>
2025-11-20 10:50:20 +0000

    removed a \

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "main" {
     {
       Environment = var.environment
     }
-  )\
-  
+  )
+
 }
 # User-Assigned Managed Identity for Container App
 resource "azurerm_user_assigned_identity" "container_identity" {
