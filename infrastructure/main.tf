65a5aa48b90f9fb1dd19ad3df8623320defce5cb
D-sharp-Major <daniel.major@kainos.com>
2025-11-20 10:19:42 +0000

    UAMI backend

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,5 +8,31 @@ resource "azurerm_resource_group" "main" {
     {
       Environment = var.environment
     }
-  )
+  )\
+  
+}
+# User-Assigned Managed Identity for Container App
+resource "azurerm_user_assigned_identity" "container_identity" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+ 
+  tags = {
+    Environment = var.environment
+    Application = var.app_name
+  }
+}
+ 
+# Role Assignment: Grant AcrPull to Managed Identity
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.container_identity.principal_id
+}
+ 
+# Role Assignment: Grant Key Vault Secrets User to Managed Identity
+resource "azurerm_role_assignment" "kv_secrets_user" {
+  scope                = data.azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.container_identity.principal_id
 }
