a160ab926c8507ba820610322d4f2a5877dc328b
D-sharp-Major <daniel.major@kainos.com>
2025-11-20 14:46:55 +0000

    remoced client certificate mode!

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -136,7 +136,6 @@ resource "azurerm_container_app" "backend" {
     external_enabled           = false
     target_port                = var.container_port
     transport                  = "http"
-    client_certificate_mode    = "ignore"
 
     traffic_weight {
       latest_revision = true
