46ba301551058c94809653e351ad4190e914cfde
D-sharp-Major <daniel.major@kainos.com>
2025-11-20 12:25:15 +0000

    UAMI backend (#55)
    Adding UAMI to the backend (may be stolen from Team 1 so hoping this
    works)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,10 +3,24 @@ resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
 
-  tags = merge(
-    var.tags,
-    {
-      Environment = var.environment
-    }
-  )
+  tags = {
+    Environment = var.environment
+    Application = var.app_name
+    ManagedBy   = "Terraform"
+  }
 }
+
+# User-Assigned Managed Identity for Container App
+resource "azurerm_user_assigned_identity" "container_identity" {
+  name                = "${var.app_name}-identity"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+ 
+  tags = {
+    Environment = var.environment
+    Application = var.app_name
+  }
+}
+
+# Note: Role assignments for ACR and Key Vault will be added later
+# when Container Apps are deployed and data sources are configured
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,3 +2,23 @@ output "resource_group_name" {
   value       = azurerm_resource_group.main.name
   description = "Name of the resource group"
 }
+
+output "resource_group_id" {
+  value       = azurerm_resource_group.main.id
+  description = "ID of the resource group"
+}
+
+output "managed_identity_principal_id" {
+  value       = azurerm_user_assigned_identity.container_identity.principal_id
+  description = "Principal ID of the managed identity (for role assignments)"
+}
+
+output "managed_identity_client_id" {
+  value       = azurerm_user_assigned_identity.container_identity.client_id
+  description = "Client ID of the managed identity (for app configuration)"
+}
+
+output "managed_identity_id" {
+  value       = azurerm_user_assigned_identity.container_identity.id
+  description = "Full resource ID of the managed identity"
+}
