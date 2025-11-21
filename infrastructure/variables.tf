893037d3842f3b7bd62c7934c4d50089a415b0e4
D-sharp-Major <daniel.major@kainos.com>
2025-11-21 11:21:43 +0000

    Last (#64)
    ## Related Ticket  
    
    
    ## Description

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -60,7 +60,7 @@ variable "container_app_environment_resource_group_name" {
 variable "container_image_tag" {
   description = "Tag of the container image to deploy"
   type        = string
-  default     = "1.0.0"
+  default     = "946fa3e"
 }
 
 variable "container_cpu" {
